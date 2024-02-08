bb6ea60bab6e9a187d7a1daecc97916b124e79d0
Gustavo.Oliveira <gustavo.oliveira@senior.com.br>
2024-02-08 10:18:51 -0300

    Aula 03 - New components, Linking Whatsapp and Finished App

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,14 +1,14 @@
-import { Image, Text, View } from "react-native";
-import {useLocalSearchParams, useNavigation} from 'expo-router';
-import { Feather } from "@expo/vector-icons";
+import { Image, Text, View } from 'react-native';
+import {Redirect, useLocalSearchParams, useNavigation} from 'expo-router';
+import { Feather } from '@expo/vector-icons';
 
-import { useCartStore } from "@/stores/cart-store";
+import { useCartStore } from '@/stores/cart-store';
 
-import { PRODUCTS } from "@/utils/data/products";
-import { formatCurrency } from "@/utils/functions/format-currency";
+import { PRODUCTS } from '@/utils/data/products';
+import { formatCurrency } from '@/utils/functions/format-currency';
 
-import { Button } from "@/components/button";
-import { LinkButton } from "@/components/link-button";
+import { Button } from '@/components/button';
+import { LinkButton } from '@/components/link-button';
 
 export default function Product() {
     const cartStore = useCartStore()
@@ -16,37 +16,45 @@ export default function Product() {
 
     const {id} = useLocalSearchParams();
 
-    const product = PRODUCTS.filter((product) => product.id === id)[0];
+    const product = PRODUCTS.find((product) => product.id === id);
 
     function handleAddToCart() {
-        cartStore.add(product)
-        navigation.goBack() 
+        if(product) {
+            cartStore.add(product)
+            navigation.goBack() 
+        }
+    }
+
+    if(!product) {
+        return <Redirect href='/'/>
     }
 
     return (
-        <View className="flex-1">
-            <Image source={product.cover} className="w-full h-52" resizeMode="cover" />
+        <View className='flex-1'>
+            <Image source={product.cover} className='w-full h-52' resizeMode='cover' />
 
-            <View className="p-5 mt-8 flex-1">
-                <Text className="text-lime-400 text-2xl font-heading my-2">
+            <View className='p-5 mt-8 flex-1'>
+                <Text className='text-white text-xl font-heading'>{product.title}</Text>
+                
+                <Text className='text-lime-400 text-2xl font-heading my-2'>
                     {formatCurrency(product.price)}
                 </Text>
 
-                <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+                <Text className='text-slate-400 font-body text-base leading-6 mb-6'>
                     {product.description}
                 </Text>
 
                 {product.ingredients.map((ingredient) => (
-                    <Text className="text-slate-400 font-body text-base leading-6" key={ingredient}>
+                    <Text className='text-slate-400 font-body text-base leading-6' key={ingredient}>
                         {'\u2022'} {ingredient}
                     </Text>
                 ))}
             </View>
 
-            <View className="p-5 pb-8 gap-5">
+            <View className='p-5 pb-8 gap-5'>
                 <Button onPress={handleAddToCart}>
                     <Button.Icon>
-                        <Feather name="plus-circle" size={20} />
+                        <Feather name='plus-circle' size={20} />
                     </Button.Icon>
 
                     <Button.Text>
@@ -54,7 +62,7 @@ export default function Product() {
                     </Button.Text>
                 </Button>
 
-                <LinkButton title="Voltar ao cardápio" href="/" />
+                <LinkButton title='Voltar ao cardápio' href='/' />
             </View>
         </View>
     )
